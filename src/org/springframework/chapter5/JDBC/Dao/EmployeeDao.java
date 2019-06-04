1987cb67bf8ca8b6f98ebafb10a4b81f74cd33ca
sakait <sakaitbhandari@outlook.com>
2019-06-05 05:11:31 +0530

    Adding init :

--- src/org/springframework/chapter5/JDBC/Dao/EmployeeDao.java
+++ src/org/springframework/chapter5/JDBC/Dao/EmployeeDao.java
@@ -1,6 +1,6 @@
 package org.springframework.chapter5.JDBC.Dao;
 
-import org.springframework.chapter2.constructioninjection.simplejavatypes.Employee;
+import org.springframework.chapter5.JDBC.Model.Employee;
 
 public interface EmployeeDao {
     void createEmployee();
--- src/org/springframework/chapter5/JDBC/Dao/EmployeeDaoImpl.java
+++ src/org/springframework/chapter5/JDBC/Dao/EmployeeDaoImpl.java
@@ -1,11 +1,8 @@
 package org.springframework.chapter5.JDBC.Dao;
 
-import org.springframework.chapter2.constructioninjection.simplejavatypes.Employee;
+import org.springframework.chapter5.JDBC.Model.Employee;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class EmployeeDaoImpl implements EmployeeDao{
 
@@ -14,9 +11,10 @@ public class EmployeeDaoImpl implements EmployeeDao{
 
     private void registerDriver(){
         try{
-            Class.forName(DB_DRIVER);
-        }
-        catch(Exception e){
+            Class.forName(DB_DRIVER).newInstance();
+        } catch (InstantiationException e) {
+        } catch (IllegalAccessException e) {
+        } catch (ClassNotFoundException e) {
         }
     }
 
@@ -27,11 +25,11 @@ public class EmployeeDaoImpl implements EmployeeDao{
             registerDriver();
             conn = DriverManager.getConnection(DB_URL);
             Statement stmt = conn.createStatement();
-            stmt.execute("create table employee_1 (id integer,name char(30))");
+            stmt.executeUpdate("create table employee_1 (id integer,name char(30))");
             stmt.close();
         }
         catch(SQLException e){
-
+            System.out.println(e);
         }
         finally {
                 if(conn != null){
@@ -39,6 +37,7 @@ public class EmployeeDaoImpl implements EmployeeDao{
                         conn.close();
                     }
                     catch (SQLException e){
+                        System.out.println(e);
                     }
                 }
         }
@@ -46,12 +45,60 @@ public class EmployeeDaoImpl implements EmployeeDao{
 
     @Override
     public Employee getEmployeeById(int id) {
-        return null;
+
+        Connection conn = null;
+        Employee employee = null;
+        try{
+            registerDriver();
+            conn = DriverManager.getConnection(DB_URL);
+            PreparedStatement ps = conn.prepareStatement("select * from employee_1 where id=?");
+            ps.setInt(1,id);
+            ResultSet rs = ps.executeQuery();
+            if( rs.next()){
+                employee = new Employee(rs.getString("name"),id);
+            }
+            rs.close();
+            ps.close();
+        }
+        catch(SQLException e){
+            System.out.println(e);
+        }
+        finally {
+            if(conn != null){
+                try{
+                    conn.close();
+                }
+                catch (SQLException e){
+                    System.out.println(e);
+                }
+            }
+        }
+        return employee;
     }
 
     @Override
     public void insertEmployee(Employee employee) {
-
+        Connection conn = null;
+        try{
+            registerDriver();
+            conn = DriverManager.getConnection(DB_URL);
+            Statement stmt = conn.createStatement();
+            stmt.execute("insert into employee_1 values("+employee.getId()+",'"+employee.getName()+"')");
+            stmt.close();
+        }
+        catch (SQLException e){
+            System.out.println(e);
+        }
+        finally {
+            if(conn != null){
+                try{
+                    conn.close();
+                }
+                catch (SQLException e){
+                    System.out.println(e);
+                }
+            }
+        }
     }
 }
 
